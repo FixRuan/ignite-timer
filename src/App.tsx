2d898f1d7ab9ebbd32381d514569880b5f119c5f
Ruan Pablo <ruan_goio10@hotmail.com>
2022-07-11 12:52:32 -0300

    styled-components types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/theme/default';
 
 
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>
+
+      </div>
+    </ThemeProvider>
   )
 }
