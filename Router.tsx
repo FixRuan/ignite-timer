a1d1406e8b3cc44dfbe376e398cb4f0acd235069
1SyuLi <ruan_goio10@hotmail.com>
2022-07-11 15:22:53 -0300

    Router - (Header layout)

--- Router.tsx
+++ Router.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './src/pages/Home'
 import { History } from './src/pages/History'
+import { DefaultLayout } from './src/layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
